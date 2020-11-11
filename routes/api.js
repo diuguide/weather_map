5120cbfdf4fdf0331b8b48f95741215b7f69ddd0
diuguide <61917141+diuguide@users.noreply.github.com>
2020-11-11 09:42:14 -0500

    PUT request for updateHome, updateRecentSearch

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const router = express.Router();
 
@@ -23,6 +24,46 @@ router.delete('/User/:id', (req, res) => {
     User.findOneAndDelete({ '_id': req.params.id })
     .then(data => res.json(data))
     .catch(err => console.log('Error DELETE: ', err))
+});
+
+// Update Home
+router.put('/updateHome', (req, res) => {
+    User.findOneAndUpdate(
+      { username: req.body.username },
+      { home: req.body.home },
+      { safe: true, upsert: true },
+      (error, data) => {
+        if(error){
+          console.log("error PUT: ", error)
+        } else {
+          res.send(data);
+          console.log("Success PUT", data)
+        }
+      }
+    ).then(data => {
+      console.log("Data promise: ", data);
+    })
+    .catch(err => console.log("Error Catch: ", err))
+});
+
+// Update recent search
+router.put('/updateRecentSearch', (req, res) => {
+  User.findOneAndUpdate(
+    { username: req.body.username },
+    { recent_search: req.body.recent_search },
+    { safe: true, upsert: true },
+    (error, data) => {
+      if(error){
+        console.log("error PUT: ", error)
+      } else {
+        res.send(data);
+        console.log("Success PUT", data)
+      }
+    }
+  ).then(data => {
+    console.log("Data promise: ", data);
+  })
+  .catch(err => console.log("Error Catch: ", err))
 });
 
 module.exports = router;
