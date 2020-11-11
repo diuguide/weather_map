6051372e247b5c3d11dce72d6dd00bc95709e8be
diuguide <61917141+diuguide@users.noreply.github.com>
2020-11-11 15:38:12 -0500

    cleaned up client, installed bootstrap, concurrently etc

--- routes/api.js
+++ routes/api.js
@@ -6,46 +6,46 @@ const User = require('../models/Model');
 
 // Get All User Data
 router.get('/', (req, res) => {
-    User.find({})
+  User.find({})
     .then(data => res.json(data))
     .catch(err => console.log('Error GET: ', err));
 })
 
 // Create New User
 router.post('/User', (req, res) => {
-    User.create(req.body)
+  User.create(req.body)
     .then(data => {
-        res.json(data)
-        console.log('Success PUT: ', req.body);
+      res.json(data)
+      console.log('Success PUT: ', req.body);
     })
     .catch(err => console.log('Error PUT: ', err));
 });
 
 // Delete User By ID
 router.delete('/User/:id', (req, res) => {
-    User.findOneAndDelete({ '_id': req.params.id })
+  User.findOneAndDelete({ '_id': req.params.id })
     .then(data => res.json(data))
     .catch(err => console.log('Error DELETE: ', err))
 });
 
 // Update Home
 router.put('/updateHome', (req, res) => {
-    User.findOneAndUpdate(
-      { username: req.body.username },
-      { home: req.body.home },
-      { safe: true, new: true },
-      (error, data) => {
-        if(error){
-          console.log("error PUT: ", error)
-        } else if (data === null) {
-          res.send("Username does not exsist");
-          console.log("Username does not exsist");
-        } else {
-          res.send(data);
-          console.log("Home Updated: ", data)
-        }
+  User.findOneAndUpdate(
+    { username: req.body.username },
+    { home: req.body.home },
+    { safe: true, new: true },
+    (error, data) => {
+      if (error) {
+        console.log("error PUT: ", error)
+      } else if (data === null) {
+        res.send("Username does not exsist");
+        console.log("Username does not exsist");
+      } else {
+        res.send(data);
+        console.log("Home Updated: ", data)
       }
-    ).catch(err => console.log("Error Catch: ", err))
+    }
+  ).catch(err => console.log("Error Catch: ", err))
 });
 
 // Update Recent Search
@@ -55,7 +55,7 @@ router.put('/updateRecentSearch', (req, res) => {
     { $addToSet: { recent_search: req.body.recent_search } },
     { safe: true, new: true },
     (error, data) => {
-      if(error){
+      if (error) {
         console.log("error PUT: ", error)
       } else if (data === null) {
         res.send("Username does not exsist");
