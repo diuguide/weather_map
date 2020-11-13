0e3cbad2e8b1567c4f570f1a519957ef9ec5d6e4
diuguide <61917141+diuguide@users.noreply.github.com>
2020-11-13 02:21:33 -0500

    created actions

---
+++ client/src/actions/types.js
@@ -0,0 +1,2 @@
+export const GET_SEARCH_QUERY = 'GET_SEARCH_QUERY';
+export const RECENT_SEARCH = 'RECENT_SEARCH';
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers';
+import store from './store';
 import './index.css';
 import App from './App';
 
-const store = createStore(rootReducer);
+
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-);
+); 
