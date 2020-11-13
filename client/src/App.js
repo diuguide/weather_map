ec45646baa0eb6eff8f19da94107695814cd7b04
diuguide <61917141+diuguide@users.noreply.github.com>
2020-11-12 23:58:09 -0500

    add go button

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import SearchBar from './components/SearchBar';
 import './App.css';
 
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  
   return (
     <div className="container-fluid"> 
       <SearchBar />
--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -4,8 +4,9 @@ import Form from 'react-bootstrap/Form';
 function SearchBar() {
     return (
         <div className="searchBar">
-            <Form.Group controlId="formBasicSearch">
+            <Form.Group className="d-inline-flex" controlId="formBasicSearch">
                 <Form.Control type="search" placeholder="Enter City" />
+                <Button variant="dark" type="submit">GO</Button>
             </Form.Group>
         </div>
     )
