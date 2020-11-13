2e56ed9d4f9a5425f8d7c2aec7902ac9fa1a3c74
diuguide <61917141+diuguide@users.noreply.github.com>
2020-11-13 00:42:20 -0500

    set up current weather axios call

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function SearchBar() {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleChange = (e) => setSearchQuery(e.target.value);
+
+    const handleSubmit = () => {
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&id=524901&appid=08bea1b85d0458c294c28493bcc4e4fe&units=imperial`)
+            .then(response => {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    }
     return (
         <div className="searchBar">
             <Form.Group className="d-inline-flex" controlId="formBasicSearch">
-                <Form.Control type="search" placeholder="Enter City" />
-                <Button variant="dark" type="submit">GO</Button>
+                <Form.Control onChange={handleChange} value={searchQuery} type="search" placeholder="Enter City" />
+                <Button onClick={handleSubmit} variant="dark" type="submit">GO</Button>
             </Form.Group>
         </div>
     )
---
+++ client/src/components/resultsTest/index.js
@@ -0,0 +1,12 @@
+import { Row, Col } from 'react-bootstrap';
+
+const resultsTest = () => {
+    return(
+        <Row>
+            <Col>
+            
+            </Col>
+        </Row>
+    )
+}
+
