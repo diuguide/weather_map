de7639a6e14f2c63094d386d0448ee67ce59048d
everett <everett.diuguid@gmail.com>
2020-11-19 23:51:01 -0500

    styled

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -9,41 +9,41 @@ const Results = () => {
     <>
         <Row className="d-inline-flex results">
           <Col>
-            <Card className="ml-1 tempCard">
-              <Card.Header as="h5">Current Temp</Card.Header>
-              <div className="temp">{dataSet.main.temp} &deg;F</div>
+            <Card className="ml-1 shadow tempCard">
+              <Card.Header className="text-center" as="h5">Current Temp</Card.Header>
+              <div className="temp">{dataSet.main.temp}<span className="fSymbol">&deg;F</span></div>
             </Card>
           </Col>
           <Col>
-            <Card className="ml-1 tempCard">
-              <Card.Header as="h5">Max</Card.Header>
-              <div className="temp">{dataSet.main.temp_max} &deg;F</div>
+            <Card className="ml-1 shadow tempCard">
+              <Card.Header className="text-center" as="h5">Max</Card.Header>
+              <div className="temp">{dataSet.main.temp_max}<span className="fSymbol">&deg;F</span></div>
             </Card>
           </Col>
           <Col>
-            <Card className="ml-1 tempCard">
-              <Card.Header as="h5">Min</Card.Header>
-              <div className="temp">{dataSet.main.temp_min} &deg;F</div>
+            <Card className="ml-1 shadow tempCard">
+              <Card.Header className="text-center" as="h5">Min</Card.Header>
+              <div className="temp">{dataSet.main.temp_min}<span className="fSymbol">&deg;F</span></div>
             </Card>
           </Col>
         </Row>
         <Row className="d-inline-flex mt-3 results">
           <Col>
-            <Card className="ml-1 tempCard">
-              <Card.Header as="h5">Pressure</Card.Header>
-              <div className="temp">{dataSet.main.pressure} &deg;F</div>
+            <Card className="ml-1 shadow tempCard">
+              <Card.Header className="text-center" as="h5">Pressure</Card.Header>
+              <div className="temp">{dataSet.main.pressure}<span className="mbarSymbol">mbar</span></div>
             </Card>
           </Col>
           <Col>
-            <Card className="ml-1 tempCard">
-              <Card.Header as="h5">Humidity</Card.Header>
-              <div className="temp">{dataSet.main.humidity} &deg;F</div>
+            <Card className="ml-1 shadow tempCard">
+              <Card.Header className="text-center" as="h5">Humidity</Card.Header>
+              <div className="temp">{dataSet.main.humidity}<span className="degreeSymbol">%</span></div>
             </Card>
           </Col>
           <Col>
-            <Card className="ml-1 tempCard">
-              <Card.Header as="h5">Wind Speed</Card.Header>
-              <div className="temp">{dataSet.wind.speed} &deg;F</div>
+            <Card className="ml-1 shadow tempCard">
+              <Card.Header className="text-center" as="h5">Wind Speed</Card.Header>
+              <div className="temp">{dataSet.wind.speed}<span className="mphSymbol">mph</span></div>
             </Card>
           </Col>
         </Row>
