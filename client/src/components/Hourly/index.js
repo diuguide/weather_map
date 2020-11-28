4acf32d578a971e6a1cea118ed85b52e900c8221
everett <everett.diuguid@gmail.com>
2020-11-28 12:05:13 -0500

    hourly table created and mapped

--- client/src/components/Hourly/index.js
+++ client/src/components/Hourly/index.js
@@ -4,13 +4,13 @@ import { useSelector, shallowEqual } from "react-redux";
 const Hourly = () => {
   const store = useSelector((store) => store, shallowEqual);
   const dataSet = store.searchQuery.search_data.data.hourly;
-  console.log('dataSet: ', dataSet)
+  const dataSetMap = dataSet.filter((data, i) => i < 12);
   
   return (
     <Container>
       <Row>
         <Col>
-          <Table striped bordered hover size="sm">
+          <Table className="shadow hourlyTable" striped bordered hover size="sm">
             <thead>
               <tr>
                 <th>Time</th>
@@ -20,12 +20,16 @@ const Hourly = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
+              {dataSetMap.map((data, i) => {
+                return (
+                  <tr key={i}>
+                  <td>{data.dt}</td>
+                  <td>{data.feels_like}</td>
+                  <td>{data.humidity}</td>
+                  <td>{data.wind_speed}</td>
+                </tr>
+                )
+              })}
             </tbody>
           </Table>
         </Col>
