255ea4cb6a9d1a5eab8513674dbb5c64bfa610fd
everett <everett.diuguid@gmail.com>
2020-12-07 08:07:08 -0500

    modified if env production file in server.js simplified package.json

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ const port = process.env.PORT || 5000;
 //
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-  });
 }
 
 // connect database
